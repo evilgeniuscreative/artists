06c8c0e0ddc93b2e9e8dcc6104449ab3496bbd93
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-03-14 23:39:59 -0400

    attempting to add Yup and Formik and Validation

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import './inputForm.css';
 import {
   Formik,
+  useFormik,
+  useField,
+  FormikProvider,
   FormikHelpers,
   FormikProps,
   Form,
@@ -9,18 +12,31 @@ import {
   FieldProps,
 } from 'formik';
 
+import * as Yup from 'yup';
+
 interface MyFormValues {
   value: string;
   description: string;
 }
 
+const AddNewSchema = Yup.object().shape({
+  value: Yup.string()
+  .min(4, 'Must be at least 4 characters')
+  .max(70, 'Must be less than 70 characters')
+    .required('Required'),
+  description: Yup.string()
+    .required('Required')
+});
+
 const InputForm: React.FC<{}> = () => {
   const initialValues: MyFormValues = { value: '' , description: ''};
+
   return (
     <main id="new_artist">
       <h1>Add New Artist</h1>
       <Formik
         initialValues={initialValues}
+        validationSchema={AddNewSchema}
         onSubmit={(values, actions) => {
           console.log({ values, actions });
           alert(JSON.stringify(values, null, 2));
@@ -30,8 +46,10 @@ const InputForm: React.FC<{}> = () => {
         <Form>
           <label htmlFor="value">Value</label>
           <Field id="value" name="value" placeholder="Value" />
+
           <label htmlFor="description">Description</label>
-          <Field id="description" name="description" placeholder="Description" />
+          <Field id="description" name="description" placeholder="Description"/>  
+                 
           <button type="submit">Submit</button>
         </Form>
       </Formik>
