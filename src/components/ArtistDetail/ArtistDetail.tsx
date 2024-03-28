3bcb4e102845e3c20aa27c9aa09e2b90b1df091b
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-03-28 10:53:15 -0400

    ArtistDetail page working

--- src/components/ArtistDetail/ArtistDetail.tsx
+++ src/components/ArtistDetail/ArtistDetail.tsx
@@ -3,27 +3,25 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { Navigation } from '../Navigation';
 
-  type Artist = {
-    id: string;
-    name: string;
-    description: string;
-    albums: string[];
-    image: string;
-    url: string;
-  };
-  
+type Artist = {
+  id: string;
+  name: string;
+  description: string;
+  albums: string[];
+  image: string;
+  url: string;
+};
+
 const ArtistDetail = () => {
   const queryParameters = new URLSearchParams(window.location.search);
   const artistId = queryParameters.get('id');
   console.log('artistId: ', artistId);
-  
+
   const [data, setData] = useState<any[]>([]);
   const [currentSource, setCurrentSource] = useState('http://localhost:5000/artists/');
-
+  const [artistData, setArtistData] = useState<Artist[]>([]);
   const { state } = useLocation();
   const navigate = useNavigate();
-
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,13 +29,14 @@ const ArtistDetail = () => {
         //TODO: change to state.src later
         const { data } = await axios.get(currentSource);
 
-        const artistData = data.filter((data: Artist[]) => {
+        const artistData = data.filter((data: Artist) => {
+          console.log('data artist: ', data);
           return data.id === artistId;
         });
 
         console.log('artistData: ', artistData);
 
-        setData(data);
+        setArtistData(artistData);
       } catch (error) {
         console.log('Error fetch data in ArtistDetail');
       }
@@ -46,21 +45,30 @@ const ArtistDetail = () => {
     fetchData();
   }, []);
 
-  const modifiedData = data.map((data) => ({ artistId: data.id, value: data.name, description: data.description }));
+  console.log('artistData a', artistData);
+  const modifiedData = artistData.map((d) => ({ artistId: d.id, name: d.name, description: d.description, image: d.image, url: d.url }));
 
   return (
     <div>
       <Navigation />
-      <h1>Artist Detail</h1>
 
-      {modifiedData.map((artist, index) => {
-        return (
-          <section key={artistId}>
-            <header>{artist.value}</header>
-            <footer></footer>
-          </section>
-        );
-      })}
+      <main>
+        <h1>Artist Detail</h1>
+
+        {modifiedData.map((artist, index) => {
+          return (
+            <section key={artistId}>
+              <header>
+                <h2>{artist.name}</h2>
+              </header>
+              <article>
+                <p>{artist.description}</p>
+              </article>
+              <footer></footer>
+            </section>
+          );
+        })}
+      </main>
     </div>
   );
 };
