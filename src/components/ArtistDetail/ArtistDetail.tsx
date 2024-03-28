2d8c501668eeb4691cfbf562982cebb66d069a29
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-03-27 22:33:19 -0400

    trying to create artist detail page and track page but track page wont save and artist detail shows all artists

--- src/components/ArtistDetail/ArtistDetail.tsx
+++ src/components/ArtistDetail/ArtistDetail.tsx
@@ -3,15 +3,27 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { Navigation } from '../Navigation';
 
+  type Artist = {
+    id: string;
+    name: string;
+    description: string;
+    albums: string[];
+    image: string;
+    url: string;
+  };
+  
 const ArtistDetail = () => {
   const queryParameters = new URLSearchParams(window.location.search);
   const artistId = queryParameters.get('id');
   console.log('artistId: ', artistId);
+  
   const [data, setData] = useState<any[]>([]);
   const [currentSource, setCurrentSource] = useState('http://localhost:5000/artists/');
 
   const { state } = useLocation();
   const navigate = useNavigate();
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +31,7 @@ const ArtistDetail = () => {
         //TODO: change to state.src later
         const { data } = await axios.get(currentSource);
 
-        const artistData = data.filter((d: any[]) => {
+        const artistData = data.filter((data: Artist[]) => {
           return data.id === artistId;
         });
 
